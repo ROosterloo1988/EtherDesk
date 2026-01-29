97dca93b47093e7174ef6c668db1d1f54b6fdf73
ROosterloo1988 <117975611+ROosterloo1988@users.noreply.github.com>
2026-01-29 11:32:28 +0100

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "--- STARTING ETHERDESK ---"
+
+# 1. Start de Harvester in de achtergrond (& is VERPLICHT!)
 python harvester.py &
+
+# 2. Start de Webserver (Flask) op de voorgrond
 python web.py
